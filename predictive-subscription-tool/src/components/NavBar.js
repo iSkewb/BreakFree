ce2c333813fe0ea29cdc40118ec6255cb1c1e833
LukeConran <145418221+LukeConran@users.noreply.github.com>
2025-01-25 17:31:32 -0600

    logo adedd

--- predictive-subscription-tool/src/components/NavBar.js
+++ predictive-subscription-tool/src/components/NavBar.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import './NavBar.css'; // Import the CSS file
+import logo from '../images/1.png'; // Import the logo image
 
 const NavBar = () => {
   return (
     <div className="navbar">
+      <img src={logo} alt="Logo" className="logo" />
       <ul>
         <li><Link to="/dashboard">Dashboard</Link></li>
         <li><Link to="/add-subscription">Subscriptions</Link></li>
